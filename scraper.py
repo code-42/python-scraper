ad89289da7b2ba8bb125a1022fb036cd1eb7ab56
Edward Dupre <edwd42@gmail.com>
2019-01-21 21:01:12 -0500

    add WebDriverWait() and navigatepassword

--- scraper.py
+++ scraper.py
@@ -10,6 +10,7 @@ import time
 import os
 
 driver = webdriver.Firefox()
+wait = WebDriverWait(driver, 10) # seconds
 
 loginUrl = os.environ.get('YAHOO_LOGIN_URL')
 username = os.environ.get('YAHOO_USERNAME')
@@ -30,14 +31,27 @@ def login(driver):
         print(loginUrl)
         driver.get(loginUrl)
     
+        signIn = wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="login-username"]')))
         elem = driver.find_element_by_xpath('//*[@id="login-username"]')
         elem.clear()
         elem.send_keys(username)
+        elem = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="login-signin"]')))
+        elem = driver.find_element_by_xpath('//*[@id="login-signin"]').click()
+
+        elem = wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="login-passwd"]')))
+        elem = driver.find_element_by_xpath('//*[@id="login-passwd"]')
+        elem.clear()
+        elem.send_keys(password)
+        elem = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="login-signin"]')))
+        elem = driver.find_element_by_xpath('//*[@id="login-signin"]').click()
+       
         return driver
 
     except Exception as e:
-        print(str(e))
-        # pass
+        # print(str(e))
+        pass
+    except TimeoutException:
+        pass
 
 login(driver)
 # close and quit driver
